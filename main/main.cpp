093c3c437f7848a962b44a83fbd52857896a9a0d
eishin-nakano <eishinnakano124@gmail.com>
2025-11-19 12:57:12 +0900

    chore: comment out random gen

--- main/main.cpp
+++ main/main.cpp
@@ -434,9 +434,9 @@ int main(int argc, char *argv[])
 	readCiphertext((std::to_string(N) + "/ciphertext").c_str(), ciphertext);
 	// saveSecretKeyToTxt("secret_key_value.txt", secretKey);
 	
-	std::random_device rd;
+	// std::random_device rd;
 	// std::mt19937_64 gen(rd());
-	std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
+	// std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
 	// for (size_t i = 0; i < N; ++i) secretKey[i] = dist(gen);
 
 	
